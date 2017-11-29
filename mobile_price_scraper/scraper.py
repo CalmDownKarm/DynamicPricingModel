a330b29dd43f7ce2a4eea893224bf1eb52129473
Hitkul <hitkuljangid@gmail.com>
2017-11-29 18:35:51 +0530

    ugly scraper - if it works it works

---
+++ mobile_price_scraper/scraper.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def spider(max_pages):
+    page = 1
+    while page <= max_pages:
+        if page ==1:
+           url = 'http://www.mysmartprice.com/mobile/pricelist/mobile-price-list-in-india.html#subcategory=mobile'
+        else:
+           url = 'http://www.mysmartprice.com/mobile/pricelist/pages/mobile-price-list-in-india-'+str(page)+'.html#subcategory=mobile'
+        source_code = requests.get(url, allow_redirects=False)
+        plain_text = source_code.text.encode('ascii', 'replace')
+        soup = BeautifulSoup(plain_text,'html.parser')
+        mobile_detail = list()
+        for mobile in soup.findAll(['a','span'],{'class':['prdct-item__name','prdct-item__prc-val']}):
+            mobile_data = mobile.text.strip()
+            mobile_detail.append(mobile_data)
+            if len(mobile_detail) == 2:
+                print(mobile_detail)
+                mobile_detail = []
+        page+=1
+
+
+spider(1)
