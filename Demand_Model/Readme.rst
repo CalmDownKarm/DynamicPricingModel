43b03c4b21881168f2e48779db88af4c346c3575
Hitkul <hitkuljangid@gmail.com>
2017-12-05 20:38:43 +0530

    at the end of the day is the end of the day

---
+++ Demand_Model/Readme.rst
@@ -0,0 +1,18 @@
+Recommend using anaconda and python 3.
+Technically only the following packages are required
+* matplotlib
+* seaborn
+* numpy
+* sklearn
+* pandas
+* fbprophet
+
+
+however, if the exact environment is to be replicated
+use the command
+
+conda env create -f environment.yml 
+
+and then source activate py35 (Linux/OSX)
+or activate py35 (Windows)
+Code is present in a jupyter notebook
--- mobile_price_scraper/scraper.py
+++ mobile_price_scraper/scraper.py
@@ -1,3 +1,6 @@
+
+#web scraper for scraping prices of all mobile phones from mysmartprice.com
+# hacked it down in few min to fetch data for our model, so not a very clean implementation 
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,6 +10,7 @@ def spider(max_pages):
     f = open('mobile_price.csv','w')
     while page <= max_pages:
         print(page)
+        #creating url
         if page ==1:
            url = 'http://www.mysmartprice.com/mobile/pricelist/mobile-price-list-in-india.html#subcategory=mobile'
         else:
@@ -15,15 +19,15 @@ def spider(max_pages):
         plain_text = source_code.text.encode('ascii', 'replace')
         soup = BeautifulSoup(plain_text,'html.parser')
         mobile_detail = list()
-        for mobile in soup.findAll(['a','span'],{'class':['prdct-item__name','prdct-item__prc-val']}):
+        for mobile in soup.findAll(['a','span'],{'class':['prdct-item__name','prdct-item__prc-val']}):#pulling out all the mobile name and prices
             mobile_data = mobile.text.strip().replace(",","")
             mobile_detail.append(mobile_data)
             if len(mobile_detail) == 2:
-                f.write(mobile_detail[0]+","+mobile_detail[1]+"\n")
+                f.write(mobile_detail[0]+","+mobile_detail[1]+"\n")#writing to a file
                 # print(mobile_detail)
                 mobile_detail = []
         page+=1
     f.close()
 
 
-spider(115)
+spider(115)#115 were total number of pages on website at the time of bulding thsi scraper may change in future
